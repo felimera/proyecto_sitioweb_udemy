56ff290083e983190eb1a7409bd7da92769cb06a
andres.mera <feli041089@gmail.com>
2023-04-18 16:10:53 -0500

    Barra de navegacion II

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    let posicion = $("#pr").offset();
+    if ($(window).scrollTop() >= posicion.top) {
+      $("header").removeClass("transparente");
+    } else {
+      $("header").addClass("transparente");
+    }
+  });
+});
