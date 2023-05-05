39220473a8332f137309a1ca9393e7c13baf8e20
andres.mera <feli041089@gmail.com>
2023-05-05 10:43:55 -0500

    Proyecto 2 Carousel III JavaScript

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,9 @@
 let overlay = document.querySelector(".overlay");
 let carousel = document.querySelector(".carousel");
 let slides = document.querySelectorAll(".carousel .slide");
+let leftArrow = document.querySelector(".left-arrow");
+let rigthArrow = document.querySelector(".rigth-arrow");
+let couter = 0;
 
 function loadSlides() {
   for (i = 0; i < slides.length; i++) {
@@ -20,7 +23,30 @@ function closeModal() {
 }
 
 function nextSlide() {
+  updateArrowsState(1);
   for (i = 0; i < slides.length; i++) {
     slides[i].style.left = slides[i].offsetLeft + carousel.offsetWidth + "px";
+  }
+}
+
+function prevSlide() {
+  updateArrowsState(-1);
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.left = slides[i].offsetLeft - carousel.offsetWidth + "px";
+  }
+}
+
+function updateArrowsState(e) {
+  couter += e;
+  if (couter !== 0) {
+    leftArrow.style.display = "flex";
+  } else {
+    leftArrow.style.display = "none";
+  }
+  
+  if (couter === slides.length - 1) {
+    rigthArrow.style.display = "none";
+  } else {
+    rigthArrow.style.display = "flex";
   }
 }
