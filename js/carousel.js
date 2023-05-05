e1955933af17c34f527499af2269beb382d5f55a
andres.mera <feli041089@gmail.com>
2023-05-05 10:09:57 -0500

    Proyecto 2 Carousel II JavaScript

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,26 @@
 let overlay = document.querySelector(".overlay");
 let carousel = document.querySelector(".carousel");
+let slides = document.querySelectorAll(".carousel .slide");
+
+function loadSlides() {
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.left = carousel.offsetWidth * -i + "px";
+  }
+}
 
 function openModal() {
   overlay.style.display = "block";
   carousel.style.display = "block";
+  loadSlides();
 }
+
 function closeModal() {
   overlay.style.display = "none";
   carousel.style.display = "none";
+}
+
+function nextSlide() {
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.left = slides[i].offsetLeft + carousel.offsetWidth + "px";
+  }
 }
